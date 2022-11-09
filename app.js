5b80cceb7eef266c5a4c48552a8cfb1926d1b1f9
Hamza Zeghari <zeghari.hamzaa@gmail.com>
2022-11-09 15:07:17 +0100

    Update : Get done

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 4047
+
+const { Octokit } = require("@octokit/core")
+
+require('dotenv-flow').config()
+
+app.get('/', async (req, res) => {
+  // Octokit.js
+  // https://github.com/octokit/core.js#readme
+  const octokit = new Octokit({
+    auth: process.env.authKEY,
+  });
+
+  const { data } = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+    owner: "vercel",
+    repo: "next.js",
+    path: "examples",
+  });
+
+  // console.log("done>> ", data);
+
+  // const fitredTemplatesName = data.forEach(function (arrayData) {
+  //   console.log(arrayData.name);
+  //   return arrayData.name;
+  // });
+
+  let result = data.map(a => a.name);
+
+  console.log("fitredTemplatesName>> ", result);
+  
+  res.send(await result);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
